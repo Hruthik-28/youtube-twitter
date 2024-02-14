b62953456acc3236452b215500095e3f264a5047
HRUTHIK <hruthik9980819630@gmail.com>
2024-02-14 13:59:33 +0530

    Increase payload size limits for file uploads. Adjusted the configuration of express.json() and express.urlencoded() middleware to increase the payload size limit to 50MB

--- src/app.js
+++ src/app.js
@@ -12,8 +12,8 @@ app.use(
     })
 );
 
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 app.use(morgan("dev")); //HTTP request logger middleware for node.js 
